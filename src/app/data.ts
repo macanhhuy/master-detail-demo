296e1dd7ba06f1f6b57928b8326fdbf5e7a17ced
Mac Anh Huy <macanhhuydn@gmail.com>
2023-05-12 23:01:19 +0700

    update detail data

--- src/app/data.ts
+++ src/app/data.ts
@@ -9,6 +9,50 @@ export function getData(): any[] {
       type: 'Item',
       charge: '5.00',
       internalComments: 'test',
+      detail: [
+        {
+          // these attributes appear in the top level rows of the grid
+          name: 'Ireland',
+          continent: 'Europe',
+          language: 'English',
+          code: 'ie',
+          // these are used in the panel
+          population: 4000000,
+          summary: 'Master Drinkers',
+        },
+        // and then repeat for all the other countries
+        {
+          name: 'Spain',
+          continent: 'Europe',
+          language: 'Spanish',
+          code: 'es',
+          population: 4000000,
+          summary: 'Bull Fighters',
+        },
+        {
+          name: 'United Kingdom',
+          continent: 'Europe',
+          language: 'English',
+          code: 'gb',
+          population: 4000000,
+          summary: 'Center of the World',
+        },
+        {
+          name: 'France',
+          continent: 'Europe',
+          language: 'French',
+          code: 'fr',
+          population: 4000000,
+          summary: 'Best Lovers',
+        },
+        {
+          name: 'Germany',
+          continent: 'Europe',
+          language: 'German',
+          code: 'de',
+          population: 4000000,
+          summary: 'Always on Time',
+        }]
     },
     {
       activityId: '2',
@@ -19,160 +63,31 @@ export function getData(): any[] {
       type: 'Guest',
       charge: '50.00',
       internalComments: 'test',
+      detail: [{
+        name: 'Peru',
+        continent: 'South America',
+        language: 'Spanish',
+        code: 'pe',
+        population: 4000000,
+        summary: 'Paddington Bear',
+      },
+      {
+        name: 'Venezuela',
+        continent: 'South America',
+        language: 'Spanish',
+        code: 've',
+        population: 4000000,
+        summary: 'Never Been, Dunno',
+      },
+      {
+        name: 'Uruguay',
+        continent: 'South America',
+        language: 'Spanish',
+        code: 'uy',
+        population: 4000000,
+        summary: 'Excellent Food',
+      }]
     }
-  ];
-  return rowData;
-}
-
-export function getChildData(): any[] {
-  var rowData = [
-    {
-      // these attributes appear in the top level rows of the grid
-      name: 'Ireland',
-      continent: 'Europe',
-      language: 'English',
-      code: 'ie',
-      // these are used in the panel
-      population: 4000000,
-      summary: 'Master Drinkers',
-    },
-    // and then repeat for all the other countries
-    {
-      name: 'Spain',
-      continent: 'Europe',
-      language: 'Spanish',
-      code: 'es',
-      population: 4000000,
-      summary: 'Bull Fighters',
-    },
-    {
-      name: 'United Kingdom',
-      continent: 'Europe',
-      language: 'English',
-      code: 'gb',
-      population: 4000000,
-      summary: 'Center of the World',
-    },
-    {
-      name: 'France',
-      continent: 'Europe',
-      language: 'French',
-      code: 'fr',
-      population: 4000000,
-      summary: 'Best Lovers',
-    },
-    {
-      name: 'Germany',
-      continent: 'Europe',
-      language: 'German',
-      code: 'de',
-      population: 4000000,
-      summary: 'Always on Time',
-    },
-    {
-      name: 'Sweden',
-      continent: 'Europe',
-      language: 'Swedish',
-      code: 'se',
-      population: 4000000,
-      summary: 'Home of Vikings',
-    },
-    {
-      name: 'Norway',
-      continent: 'Europe',
-      language: 'Norwegian',
-      code: 'no',
-      population: 4000000,
-      summary: 'Best Vikings',
-    },
-    {
-      name: 'Italy',
-      continent: 'Europe',
-      language: 'Italian',
-      code: 'it',
-      population: 4000000,
-      summary: 'Pizza Pizza',
-    },
-    {
-      name: 'Greece',
-      continent: 'Europe',
-      language: 'Greek',
-      code: 'gr',
-      population: 4000000,
-      summary: 'Many Gods',
-    },
-    {
-      name: 'Iceland',
-      continent: 'Europe',
-      language: 'Icelandic',
-      code: 'is',
-      population: 4000000,
-      summary: 'Exploding Volcano',
-    },
-    {
-      name: 'Portugal',
-      continent: 'Europe',
-      language: 'Portuguese',
-      code: 'pt',
-      population: 4000000,
-      summary: 'Ship Builders',
-    },
-    {
-      name: 'Malta',
-      continent: 'Europe',
-      language: 'Maltese',
-      code: 'mt',
-      population: 4000000,
-      summary: 'Fishermen',
-    },
-    {
-      name: 'Brazil',
-      continent: 'South America',
-      language: 'Portuguese',
-      code: 'br',
-      population: 4000000,
-      summary: 'Best Footballers',
-    },
-    {
-      name: 'Argentina',
-      continent: 'South America',
-      language: 'Spanish',
-      code: 'ar',
-      population: 4000000,
-      summary: 'Beef Steaks',
-    },
-    {
-      name: 'Colombia',
-      continent: 'South America',
-      language: 'Spanish',
-      code: 'co',
-      population: 4000000,
-      summary: 'Wonderful Hospitality',
-    },
-    {
-      name: 'Peru',
-      continent: 'South America',
-      language: 'Spanish',
-      code: 'pe',
-      population: 4000000,
-      summary: 'Paddington Bear',
-    },
-    {
-      name: 'Venezuela',
-      continent: 'South America',
-      language: 'Spanish',
-      code: 've',
-      population: 4000000,
-      summary: 'Never Been, Dunno',
-    },
-    {
-      name: 'Uruguay',
-      continent: 'South America',
-      language: 'Spanish',
-      code: 'uy',
-      population: 4000000,
-      summary: 'Excellent Food',
-    },
   ];
   return rowData;
 }
--- src/app/full-width-cell-renderer.component.ts
+++ src/app/full-width-cell-renderer.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { ICellRendererAngularComp } from 'ag-grid-angular';
 import { ColDef, ICellRendererComp, ICellRendererParams, IsFullWidthRowParams, RowHeightParams } from 'ag-grid-community';
-import { getChildData } from './data';
 
 @Component({
   selector: 'full-width-cell',
@@ -26,19 +25,6 @@ export class FullWidthCellRenderer implements ICellRendererAngularComp {
   public data: any;
   public flag!: string;
 
-  agInit(params: ICellRendererParams): void {
-    this.data = params.node.data;
-    this.flag = `https://www.ag-grid.com/example-assets/large-flags/${this.data.code}.png`;
-  }
-
-  latinText() {
-    return '<p>Sample Text in a Paragraph</p><p>Lorem ipsum dolor sit amet, his mazim necessitatibus te, mea volutpat intellegebat at. Ea nec perpetua liberavisse, et modo rebum persius pri. Velit recteque reprimique quo at. Vis ex persius oporteat, esse voluptatum moderatius te vis. Ex agam suscipit aliquando eum. Mediocrem molestiae id pri, ei cibo facilisis mel. Ne sale nonumy sea. Et vel lorem omittam vulputate. Ne prima impedit percipitur vis, erat summo an pro. Id urbanitas deterruisset cum, at legere oportere has. No saperet lobortis elaboraret qui, alii zril at vix, nulla soluta ornatus per ad. Feugiat consequuntur vis ad, te sit quodsi persequeris, labore perpetua mei ad. Ex sea affert ullamcorper disputationi, sit nisl elit elaboraret te, quodsi doctus verear ut eam. Eu vel malis nominati, per ex melius delenit incorrupte. Partem complectitur sed in. Vix dicta tincidunt ea. Id nec urbanitas voluptaria, pri no nostro disputationi. Falli graeco salutatus pri ea.</p><p>Quo ad omnesque phaedrum principes, tale urbanitas constituam et ius, pericula consequat ad est. Ius tractatos referrentur deterruisset an, odio consequuntur sed ad. Ea molestie adipiscing adversarium eos, tale veniam sea no. Mutat nullam philosophia sed ad. Pri eu dicta consulatu, te mollis quaerendum sea. Ei doming commodo euismod vis. Cu modus aliquip inermis his, eos et eirmod regione delicata, at odio definiebas vis.</p><p>Lorem ipsum dolor sit amet, his mazim necessitatibus te, mea volutpat intellegebat at. Ea nec perpetua liberavisse, et modo rebum persius pri. Velit recteque reprimique quo at. Vis ex persius oporteat, esse voluptatum moderatius te vis. Ex agam suscipit aliquando eum. Mediocrem molestiae id pri, ei cibo facilisis mel. Ne sale nonumy sea. Et vel lorem omittam vulputate. Ne prima impedit percipitur vis, erat summo an pro. Id urbanitas deterruisset cum, at legere oportere has. No saperet lobortis elaboraret qui, alii zril at vix, nulla soluta ornatus per ad. Feugiat consequuntur vis ad, te sit quodsi persequeris, labore perpetua mei ad. Ex sea affert ullamcorper disputationi, sit nisl elit elaboraret te, quodsi doctus verear ut eam. Eu vel malis nominati, per ex melius delenit incorrupte. Partem complectitur sed in. Vix dicta tincidunt ea. Id nec urbanitas voluptaria, pri no nostro disputationi. Falli graeco salutatus pri ea.</p><p>Quo ad omnesque phaedrum principes, tale urbanitas constituam et ius, pericula consequat ad est. Ius tractatos referrentur deterruisset an, odio consequuntur sed ad. Ea molestie adipiscing adversarium eos, tale veniam sea no. Mutat nullam philosophia sed ad. Pri eu dicta consulatu, te mollis quaerendum sea. Ei doming commodo euismod vis. Cu modus aliquip inermis his, eos et eirmod regione delicata, at odio definiebas vis.</p>';
-  }
-
-  refresh(params: ICellRendererParams): boolean {
-    return false;
-  }
-
   public columnDefs: ColDef[] = [
     { field: 'name', cellRenderer: CountryCellRenderer },
     { field: 'continent' },
@@ -50,25 +36,19 @@ export class FullWidthCellRenderer implements ICellRendererAngularComp {
     resizable: true,
     filter: true,
   };
-  public rowData: any[] | null = getChildData();
-  public getRowHeight: (
-    params: RowHeightParams
-  ) => number | undefined | null = (params: RowHeightParams) => {
-    if (this.isFullWidth(params.data)) {
-      return 100;
-    }
-    return 42;
-  };
-  public isFullWidthRow: (params: IsFullWidthRowParams) => boolean = (
-    params: IsFullWidthRowParams
-  ) => {
-    return this.isFullWidth(params.rowNode.data);
-  };
+
+  public rowData: any[]  = [];
+
   public fullWidthCellRenderer: any = FullWidthCellRenderer;
 
-  isFullWidth(data: any) {
-    // return true when country is Peru, France or Italy
-    return ['Peru', 'France', 'Italy'].indexOf(data.name) >= 0;
+  agInit(params: ICellRendererParams): void {
+    this.data = params.node.data;
+    this.rowData = this.data?.detail || [];
+    this.flag = `https://www.ag-grid.com/example-assets/large-flags/${this.data.code}.png`;
+  }
+
+  refresh(params: ICellRendererParams): boolean {
+    return false;
   }
 }
 
